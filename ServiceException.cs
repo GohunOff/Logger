bf6ac79cb08ef32c4502fb1cff7622a25a4ebb2f
PrzemysławZałuska <gohunoff@gmail.com>
2026-09-19 19:26:52 +0200

    actualization

---
+++ ServiceException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Kotrak
+{
+    public class ServiceException : Exception
+    {
+        public ServiceException() { }
+
+        public ServiceException(string message)
+            : base(message) { }
+
+        public ServiceException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+
+        }
+
+    }
+}
+    
